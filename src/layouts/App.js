6a5477302c863649a277eab2b87c7b82fa42c5f2
BartoszFirana <bartosz.r.buczkowski@gmail.com>
2020-01-25 23:30:33 +0100

    Home Page has been created

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import { BrowserRouter as Router } from 'react-router-dom';
+import '../styles/App.css';
 
 import Page from './Page';
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,40 @@
 import React from 'react';
-
 import { NavLink } from 'react-router-dom';
+
+import '../styles/Home.css';
+
+import doeAvatar from '../images/doe-avatar.svg';
+import smithAvatar from '../images/smith-avatar.svg';
+
 
 const Home = () => {
     return (
         <>
-            <NavLink to="/chat/John-Doe">John Doe</NavLink>
-            <NavLink to="/chat/John-Smith">John Smith</NavLink>
-            <p>Home</p>
+            <section className="login__container">
+                <div className="logo__wrapper">
+                    Chat
+                    <div className="logo__subtitle">to JS master</div>
+                </div>
+                <div className="login__wrapper">
+                    <NavLink to="/chat/John-Doe" className="login__button gray__background">
+                        <div className="account__wrapper">
+                            <img src={doeAvatar} alt="" className="account__avatar" />
+                            <p className="account__paragraph">John Doe</p>
+                        </div>
+                    </NavLink>
+                    <NavLink to="/chat/John-Smith" className="login__button">
+                        <div className="account__wrapper">
+                            <img src={smithAvatar} alt="" className="account__avatar" />
+                            <p className="account__paragraph">John Smith</p>
+                        </div>
+                    </NavLink>
+                </div>
+                <div className="description__wrapper">
+                    <p className="description__wrapper--paraghraph">
+                        Choice your accountand start a conversation
+                    </p>
+                </div>
+            </section>
         </>
     );
 }
